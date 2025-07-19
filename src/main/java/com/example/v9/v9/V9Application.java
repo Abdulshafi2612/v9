47e93e24f2d9959bd9e89e0274d8a08080b17908
AbdulShafi <mohamedsadik763@gmail.com>
2025-07-19 22:12:30 +0300

    intial commit

---
+++ src/main/java/com/example/v9/v9/V9Application.java
@@ -0,0 +1,13 @@
+package com.example.v9.v9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V9Application.class, args);
+	}
+
+}
